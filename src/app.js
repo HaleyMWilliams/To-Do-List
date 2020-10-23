16ebfec89e774fc73937171cd8495ff0ecd292f0
HaleyMWilliams <66376569+HaleyMWilliams@users.noreply.github.com>
2020-10-22 22:37:01 -0400

    local-storage
    JS to save the to-do list to local storage

--- src/app.js
+++ src/app.js
@@ -3,9 +3,10 @@
 const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
-const filterOption = document.querySelector('.filter-todo'); 
+const filterOption = document.querySelector('.filter-todo');
 
 //Event Listeners
+document.addEventListener('DOMContentLoaded', getTodos);
 
 todoButton.addEventListener('click', addTodo);
 
@@ -25,6 +26,9 @@ const newTodo = document.createElement('li')
 newTodo.innerText = todoInput.value;
 newTodo.classList.add('todo-item');
 todoDiv.appendChild(newTodo);
+
+//DD TO DO TO LOCAL STORAGE 
+saveLocalTodos(todoInput.value);
 
 //CHECK MARK BUTTON
 const completedButton = document.createElement('button');
@@ -53,6 +57,7 @@ function deleteCheck(event) {
 if (item.classList[0] === "trash-btn"){
     const todo = item.parentElement;
     todo.classList.add("fall");
+    removeLocalTodos(todo);
     todo.addEventListener("transitionend", function() {
         todo.remove();
     })
@@ -65,19 +70,88 @@ todo.classList.toggle('completed');
 }
 }
 
-function filterTodo(event){
+function filterTodo(event) {
     const todos = todoList.childNodes;
-    todos.forEach(function(todo) {
-        switch (event.target.value) {
+    todos.forEach(function(todo){
+        switch(event.target.value){
             case "all":
                 todo.style.display = 'flex';
                 break;
             case "completed":
-                if (todo.classList.contains('completed')) {
+                if(todo.classList.contains('completed')){
                     todo.style.display = 'flex';
                 } else {
                     todo.style.display = 'none';
                 }
+                break;
+                case "uncompleted":
+                    if(!todo.classList.contains("completed")) {
+                        todo.style.display = 'flex'; 
+                    } else {
+                        todo.style.display = 'none';
+                    }
+                    break;
             }
         });
+    }
+
+    function saveLocalTodos(todo) {
+        let todos;
+
+        if(localStorage.getItem('todos') === null) {
+            todos = [];
+        } else {
+            todos = JSON.parse(localStorage.getItem('todos'));
+        }
+
+        todos.push(todo);
+        localStorage.setItem("todos", JSON.stringify(todos));
+    }
+
+    function getTodos() {
+        let todos;
+
+        if(localStorage.getItem('todos') === null) {
+            todos = [];
+        } else {
+            todos = JSON.parse(localStorage.getItem('todos'));
+        }
+
+            todos.forEach(function(todo){
+            const todoDiv = document.createElement("div")
+            todoDiv.classList.add("todo")
+
+            // CREATE LI
+            const newTodo = document.createElement('li')
+            newTodo.innerText = todo;
+            newTodo.classList.add('todo-item');
+            todoDiv.appendChild(newTodo);
+
+            //CHECK MARK BUTTON
+            const completedButton = document.createElement('button');
+            completedButton.innerHTML = '<i class="fas fa-check"></i>';
+            completedButton.classList.add("complete-btn")
+            todoDiv.appendChild(completedButton);
+
+            //CHECK TRASH BUTTON
+            const trashButton = document.createElement('button');
+            trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+            trashButton.classList.add("trash-btn")
+            todoDiv.appendChild(trashButton);
+
+            //APPEND TO LIST
+            todoList.appendChild(todoDiv);
+            })
+    }
+
+    function removeLocalTodos(todo) {
+        let todos;
+
+        if(localStorage.getItem('todos') === null) {
+            todos = [];
+        } else {
+            todos = JSON.parse(localStorage.getItem('todos'));
+        }
+        const todoIndex = todo.children[0].innerText;
+        todos.splice(todos.indexOf(todoIndex), 1);
     }
